76fb52e7671a2861c18bbb66a8a727e823fc0d09
UnknownFur <123666616+UnknownFur@users.noreply.github.com>
2024-11-26 03:35:22 +0000

    changes

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,16 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import React from 'react';
 
 const Tab1: React.FC = () => {
   return (
+    
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Tab 1 Hola</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+      <IonButton>Default</IonButton>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
